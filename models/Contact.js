6724621dc04b5502c053b053dbf2263c34458b9d
[Richard Martin] <[thetvdoctor66@gmail.com]>
2023-12-14 15:20:22 -0500

    Added phone number field to contact page

--- models/Contact.js
+++ models/Contact.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 var contactSchema = mongoose.Schema({
     name: String,
     email: String,
+    phone: String,
     message: String,
     company: String,
     dateAdded: String
