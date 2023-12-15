a90f8608803dad597c572feafeb92559a89fdb10
[Richard Martin] <[thetvdoctor66@gmail.com]>
2023-12-15 09:28:23 -0500

    Installed and setup nodemailer for contact page

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+const nodemailer = require('nodemailer')
+
 //Load DB Models
-const Contact = require('../models/Contact')
+//const Contact = require('../models/Contact')
 //const Product = require('../models/Product')
 
 /* GET home page. */
 router.get('/', function(req, res,error) {
   res.render('home', {banner: 'Home',error: false})
 });
+
 // Search Results Page
 router.get('/results',function(req,res,error){
   res.render('results', {banner: 'Results',error: false})
 })
+
 // Result to contact
 router.post('/addQuote',function(req,res,error){
   var item = req.body
   console.log(item.service)
   res.render('contact',{banner: 'Submit Quote',error: false, item})
 })
+
 // Contact Page 
 router.get('/contact',function(req,res,error){
   var item =''
@@ -26,26 +31,56 @@ router.get('/contact',function(req,res,error){
 })
 
 // Contact Submit
-router.post('/addContact',function(req,res,error){
-  var contactInfo = req.body
-  var today = new Date()
-  var date = today.getMonth()+1+'-'+(today.getDate())+'-'+today.getFullYear()
-  var newContact = new Contact({
-      name: contactInfo.name,
-      email: contactInfo.email,
-      company: contactInfo.company,
-      message: contactInfo.message,
-      dateAdded: date
+router.post('/addContact', (req, res, error) => {
+  console.log(req.body)
+  const output = `
+    <p>You have a new service request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'louisville-website@outlook.com', // generated ethereal user
+        pass: 'ABBlouisville240'  // generated ethereal password
+    }
+    // tls:{
+    //   rejectUnauthorized:false
+    // }
   })
-  newContact.save().then(Contact =>{
-    Contact === newContact;
-    res.render('addContact',{banner:'',error:false})
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Nodemailer Contact" <louisville-website@outlook.com>', // sender address
+      to: 'thetvdoctor66@gmail.com', // list of receivers
+      cc: 'thetvdoctor66@hotmail.com', // list of receivers
+      subject: 'Service Request', // Subject line
+      text: 'I need information about ABB in Louisville.', // plain text body
+      html: output // html body
+  }
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error)
+      }
+      console.log('Message sent: %s', info.messageId) 
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+
+      res.render('addContact',{banner:'Email has been sent', error: false})
   })
-  .catch
-  (function(err){
-    console.log(err)
   })
-})
 
 // Parts/Spares Page 
 router.get('/partsSpares',function(req,res,error){
